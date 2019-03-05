2035b9b8b06bff2e17bde5100e3ec68ba00b1433
Wynspeare <wynspeare@gmail.com>
2019-03-05 13:08:06 -0600

    Initial Commit - mute button set up using Font Awesome Icons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MuteButton from './components/MuteButton/MuteButton';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faVolumeMute } from '@fortawesome/free-solid-svg-icons'
+import { faVolumeUp } from '@fortawesome/free-solid-svg-icons'
+
+
+library.add(faVolumeMute, faVolumeUp)
 
 class App extends Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      isMuted : false
+    }
+  }
+
+  _toggleMuteButton() {
+    this.setState ({
+      isMuted: !this.state.isMuted
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <h3>MUTE ME</h3>
+
+        <MuteButton isMuted={this.state.isMuted} _toggleMuteButton={this._toggleMuteButton.bind(this)} />
+
         </header>
       </div>
     );
